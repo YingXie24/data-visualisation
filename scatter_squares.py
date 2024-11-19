59877abb598e60d6b805fb86fb2d60d0ce4afb6c
yingxie <cheyannexie@gmail.com>
2024-11-19 14:52:38 +0000

    Calculate data automatically.

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values =  range(1,1001)
+y_values = [x_value**2 for x_value in x_values]
 
 plt.style.use('seaborn-v0_8-pastel')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+ax.ticklabel_format(style="plain")
 
 # Set size of tick labels.
 ax.tick_params(axis="both", which="major", labelsize=14)
+
+# Set the range for each axis
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
