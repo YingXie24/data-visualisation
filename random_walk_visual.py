c71dc86b7a4c0cbd392bc11d12a82cb178697fc3
yingxie <cheyannexie@gmail.com>
2024-11-19 17:37:05 +0000

    Alter size to fill the screen.

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -6,15 +6,15 @@ from random_walk import RandomWalk
 while True:
 
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use("classic")
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15,9))
     point_order = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_order, cmap=plt.cm.Blues,
-               edgecolors="none", s=5)
+               edgecolors="none", s=1)
     
     # Emphasize the first and last points.
     ax.scatter(0, 0, c="green", edgecolors="none", s=100)
@@ -23,7 +23,7 @@ while True:
     # Remove the axes.
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-    
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
