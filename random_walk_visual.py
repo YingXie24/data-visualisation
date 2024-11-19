7fa36df644bc9ba279da15aebba8d17e126aec3b
yingxie <cheyannexie@gmail.com>
2024-11-19 15:32:21 +0000

    Plot the starting and ending points.

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -15,6 +15,10 @@ while True:
     point_order = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_order, cmap=plt.cm.Blues,
                edgecolors="none", s=5)
+    
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c="green", edgecolors="none", s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors='none', s=100)
 
     plt.show()
 
