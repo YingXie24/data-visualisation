28534dce1dc7233bba4e65134606a79424eed0bc
yingxie <cheyannexie@gmail.com>
2024-11-20 21:11:05 +0000

    Plot a longer timeframe, from one month to one year.

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -29,10 +29,12 @@ with open(filename) as f:
     ax.plot(dates, highs, c="red")
 
     # Format plot.
-    ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+    ax.set_title("Daily high temperatures, 2018", fontsize=24)
     ax.set_xlabel('', fontsize=16)
     ax.set_ylabel("Temperature (F)", fontsize=16)
     ax.tick_params(axis="both", which="major", labelsize=16)
+
+    # Draw the date labels diagonally to prevent them from overlapping.
     fig.autofmt_xdate()
 
     plt.show()
