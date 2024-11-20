1074f963b6ed9d0002b57ce2adf4b9c0b6965e43
yingxie <cheyannexie@gmail.com>
2024-11-20 21:31:40 +0000

    Check for missing data.'

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,26 +1,24 @@
-import csv 
+import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_2018_simple.csv'
-
-with open(filename) as f:
+with open('data/death_valley_2018_simple.csv') as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    ## Parse header row
-    # for index, column_header in enumerate(header_row):
-    #     print(index, column_header)
-
-    # Get dates and high temperatures from this file.
+# Get dates and high temperatures from this file.
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high and low temperatures.
 # print(plt.style.available)
@@ -33,7 +31,7 @@ ax.plot(dates, lows, c="blue")
 ax.fill_between(dates, highs, lows, facecolor='red', alpha=0.2)
 
 # Format plot.
-ax.set_title("Daily high and low temperatures, 2018", fontsize=24)
+ax.set_title("Daily high and low temperatures, 2018\nDeath Valley, CA", fontsize=20)
 ax.set_xlabel('', fontsize=16)
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(axis="both", which="major", labelsize=16)
