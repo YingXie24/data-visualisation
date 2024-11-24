b7d37f119e92de80384505e8cb1e626d39325251
yingxie <cheyannexie@gmail.com>
2024-11-24 14:42:45 +0000

    Automate plot title.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -8,9 +8,12 @@ filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-# readable_file = 'data/readable_eq_data_1_day_m1.json'
+# readable_file = 'data/readable_eq_data_30_day_m1.json'
 # with open(readable_file, 'w') as f:
 #     json.dump(all_eq_data, f, indent=4)
+
+# Get title of the dataset.
+dataset_title = all_eq_data["metadata"]["title"]
 
 # Make list of all earthquakes.
 all_eq_dicts = all_eq_data['features']
@@ -45,7 +48,8 @@ data = [{
         "colorbar": {"title": "Magnitude"}
     }
 }]
-my_layout = Layout(title="Global Earthquakes")
+
+my_layout = Layout(title=dataset_title)
 
 fig = {"data": data, "layout": my_layout}
 offline.plot(fig, filename="global_earthquakes.html")
