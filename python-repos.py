0c3d10d02641b293a06d0e3fceec149f8f03820f
yingxie <cheyannexie@gmail.com>
2024-11-24 15:32:48 +0000

    Work with the response dictionary.

--- python-repos.py
+++ python-repos.py
@@ -1,7 +1,7 @@
 import requests
 
 # Make an API call and store the response.
-url = "https://api.github.com/search/repositories?q=languange:python&sort=stars"
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 headers = {"Accept":  "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
@@ -10,6 +10,15 @@ print(f"Status code: {r.status_code}")
 #  The reposnse object is in JSON format, and the json() method converts the information 
 # to a Python dictionary.
 response_dict = r.json()
+print(f"Total repositories: {response_dict["total_count"]}")
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict["items"]
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
+
